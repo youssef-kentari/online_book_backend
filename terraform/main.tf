dd3a6fa55950f0d443a0dcd68d83ea28e33d048d
youssef-kentari <youssefkentari56@gmail.com>
2025-05-15 19:15:02 +0100

    adding terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,11 @@
 resource "null_resource" "create_kind_cluster" {
   provisioner "local-exec" {
-    command = <<EOT
-if kind get clusters | grep -q "^my_cluster$"; then
-  kind delete cluster --name my_cluster
-fi
-kind create cluster --name my_cluster --config=../create_cluster.yml
-EOT
+    command = <<-EOT
+      if kind get clusters | grep -q "^my_cluster$"; then
+        kind delete cluster --name my_cluster
+      fi
+      kind create cluster --name my_cluster --config=../create_cluster.yml
+    EOT
     interpreter = ["bash", "-c"]
   }
 
