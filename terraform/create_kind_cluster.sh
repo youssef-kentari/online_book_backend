f973c4593214215f8a94128025c1c8d14bcfb45f
youssef-kentari <youssefkentari56@gmail.com>
2025-05-17 14:11:37 +0100

    testing argoCD

--- terraform/create_kind_cluster.sh
+++ terraform/create_kind_cluster.sh
@@ -5,5 +5,5 @@ if ! kind get clusters | grep -q "^my-cluster$"; then
   echo "Creating Kind cluster..."
   kind create cluster --name my-cluster --config=../create_cluster.yml
 else
-  echo "Kind cluster already exists. Skipping creation."
+  echo "Kind cluster already exists Skipping creation."
 fi
